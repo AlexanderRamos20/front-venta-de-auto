c9c3c502d6b6cdb9c37291bac1d9687171cf96aa
Alexander Ramos <ramos.olivera.alexander@gmail.com>
2025-05-19 14:06:06 -0400

    añade funciones de calculo subtotaltes y total

--- script/script.js
+++ script/script.js
@@ -2,102 +2,117 @@ let colorSeleccionado = "blanco";
 let precioBase = 3500000;
 let subtotalColor = 0;
 let subtotalAccesorio = 0;
+let total = 0;
 
 
 function muestraImagen(event){
     const botonId = event.target.id;
     const img = document.getElementById("espacioImg");
     colorSeleccionado = botonId;
-   
+    
     if (botonId === 'blanco'){
-    img.src="images/auto-blanco.png";
-    subtotalColor = precioBase
-    calcularTotal();
+        img.src="images/auto-blanco.png";
+        subtotalColor = precioBase;
     }
     else if (botonId === 'negro'){
         img.src="images/auto-negro.png";
         subtotalColor = precioBase + 800000;
-        calcularTotal();
     }
     else if (botonId === 'rojo'){
         img.src="images/auto-rojo.png";
         subtotalColor = precioBase + 400000;
-        calcularTotal();
     }
-
-    // setTimeout(() =>{
-    //     const tallaSeleccionada = document.getElementById("tallas").value;
-    //     if (tallaSeleccionada) {
-    //         calculaPrecioTallaColor();
-    //     }
-    // } , 0);
 }
-// function reiniciarSubtotal(){
-//     document.getElementById("subtotal-text").value="";
-// }
-// function reiniciarTalla(){
-//     document.getElementById("tallas").value="blank";
-// }
 
-// function reiniciarEnvio(){
-//     document.getElementById("retiro").checked = false;
-//     document.getElementById("envio").checked = false;
-//     document.getElementById("total-text").value = "";
-// }
+document.addEventListener('DOMContentLoaded', () => {
+  subtotalColor = precioBase;
+  calcularTotal();
+});
 
-// function reiniciarElementos(){
-//     reiniciarSubtotal(); reiniciarTalla(); reiniciarEnvio()
-// }
+function calcularSubtotalAccesorios(){
+    subtotalAccesorio = 0;
+    const arregloAccesorios = document.querySelectorAll('input[name="accesorios"]');
+    arregloAccesorios.forEach(accesorio => {
+        if (accesorio.checked) {subtotalAccesorio += parseInt(accesorio.value)} 
+    });
+}
+function listenerSubtotalAccesorios(){
+    const arregloAccesorios = document.querySelectorAll('input[name="accesorios"]');
+    arregloAccesorios.forEach(accesorio => {
+        accesorio.addEventListener('change', calcularSubtotalAccesorios)
+    });
+}
+listenerSubtotalAccesorios();
 
+function reiniciarAccesorios(){
+    const arregloAccesorios = document.querySelectorAll('input[name="accesorios"]');
+    arregloAccesorios.forEach(accesorio => {
+        accesorio.checked = false;
+    });
+    calcularSubtotalAccesorios();
+    calcularTotal();
+};
 
 function calcularTotal(){
-    total = document.getElementById("subtotal-text");
-    total.value = (`$${subtotalColor + subtotalAccesorio} clp`);
+    totalInput = document.getElementById("total-text");
+    total = subtotalAccesorio + subtotalColor;
+    totalInput.value = (`$${total} clp`);
 }
 
-function confirmarPedido (event){
-    direccionEnvio = document.getElementById("direccion").value.trim();
-    const resumenCompra = {
-        color: colorSeleccionado,
-        talla: talla,
-        envio: envio,
-        direccionEnvio: direccionEnvio,
-        subtotal: subtotal,
-        total: total
-    }
+function recalcularTotal(){
+    const arregloColorAuto = document.querySelectorAll('input[name="color-auto"]');
+    const arregloAccesorios = document.querySelectorAll('input[name="accesorios"]');
+    const arregloColorAccesorios = [...arregloColorAuto, ...arregloAccesorios]
+    arregloColorAccesorios.forEach(elemento => {
+        elemento.addEventListener('change', calcularTotal);
+        console.log("paso 1")
+    });
+}
 
-    const coloresValidos = ["blanco", "negro", "rojo"];
-    const tallasValidas = ["small", "medium", "large"];
+recalcularTotal();
+// function confirmarPedido (event){
+//     direccionEnvio = document.getElementById("direccion").value.trim();
+//     const resumenCompra = {
+//         color: colorSeleccionado,
+//         talla: talla,
+//         envio: envio,
+//         direccionEnvio: direccionEnvio,
+//         subtotal: subtotal,
+//         total: total
+//     }
 
-    if (
-        coloresValidos.includes(resumenCompra.color) &&
-        tallasValidas.includes(resumenCompra.talla) &&
-        typeof resumenCompra.envio === "boolean" &&
-        Number.isInteger(resumenCompra.subtotal) && resumenCompra.subtotal> 0 &&
-        Number.isInteger(resumenCompra.total) && resumenCompra.total > 0
-    ){
-        if (resumenCompra.envio === true)
-            {
-            if (resumenCompra.direccionEnvio.trim() === "")
-                {
-                    alert ("Dirección de envío no válida, vuelva a ingresarla");
-                    return;
-                }
-            }
+//     const coloresValidos = ["blanco", "negro", "rojo"];
+//     const tallasValidas = ["small", "medium", "large"];
 
-    const mensaje = `Pedido confirmado:
-    -Color: ${resumenCompra.color}
-    -Talla: ${resumenCompra.talla}
-    -Envío: ${resumenCompra.envio ? "Sí" : "No"}
-    -direccion: ${resumenCompra.direccionEnvio ? resumenCompra.direccionEnvio: "No aplica"}
-    -Subtotal: ${resumenCompra.subtotal}
-    -Total: ${resumenCompra.total}`
+//     if (
+//         coloresValidos.includes(resumenCompra.color) &&
+//         tallasValidas.includes(resumenCompra.talla) &&
+//         typeof resumenCompra.envio === "boolean" &&
+//         Number.isInteger(resumenCompra.subtotal) && resumenCompra.subtotal> 0 &&
+//         Number.isInteger(resumenCompra.total) && resumenCompra.total > 0
+//     ){
+//         if (resumenCompra.envio === true)
+//             {
+//             if (resumenCompra.direccionEnvio.trim() === "")
+//                 {
+//                     alert ("Dirección de envío no válida, vuelva a ingresarla");
+//                     return;
+//                 }
+//             }
+
+//     const mensaje = `Pedido confirmado:
+//     -Color: ${resumenCompra.color}
+//     -Talla: ${resumenCompra.talla}
+//     -Envío: ${resumenCompra.envio ? "Sí" : "No"}
+//     -direccion: ${resumenCompra.direccionEnvio ? resumenCompra.direccionEnvio: "No aplica"}
+//     -Subtotal: ${resumenCompra.subtotal}
+//     -Total: ${resumenCompra.total}`
     
-    const mensajeCodificado = encodeURIComponent(mensaje);
-    const numeroWhatsapp = "56975848210";
+//     const mensajeCodificado = encodeURIComponent(mensaje);
+//     const numeroWhatsapp = "56975848210";
 
-        window.open(`https://wa.me/${numeroWhatsapp}?text=${mensajeCodificado}`, "_blank")
-    } else {
-                alert("Hay uno o más campos inválidos en su pedido. Favor vuela a intentarlo.")
-            }
-};
+//         window.open(`https://wa.me/${numeroWhatsapp}?text=${mensajeCodificado}`, "_blank")
+//     } else {
+//                 alert("Hay uno o más campos inválidos en su pedido. Favor vuela a intentarlo.")
+//             }
+// };
