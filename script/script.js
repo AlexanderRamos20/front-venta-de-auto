55671e990fabb8a66a2f0909121f4f3def4d5271
Alexander Ramos <ramos.olivera.alexander@gmail.com>
2025-05-19 20:12:10 -0400

    completa mensajes de alert

--- script/script.js
+++ script/script.js
@@ -79,13 +79,32 @@ function recalcularTotal(){
 recalcularTotal();
 
 function mostrarDatos(){
+    calcularSubtotalAccesorios(); 
     const infoPedido = document.getElementById("datos-pedido");
-    const mensajeAccesorios = ;
+    let mensajeAccesorios = null;
+    
+    if(listadoAccesorio.every(function(valor){
+        return !valor;
+    } )){
+        mensajeAccesorios = "No incluye"
+    } else {
+        mensajeAccesorios = ` <br>
+        Neblineros: ${listadoAccesorio[0]? "Si": "No"} <br>
+        Portaequipaje: ${listadoAccesorio[1]? "Si": "No"} <br>
+        Acc. mascota: ${listadoAccesorio[2]? "Si": "No"} <br>
+        Luces LED: ${listadoAccesorio[3]? "Si": "No"} <br>
+        Cámara trasera: ${listadoAccesorio[4]? "Si": "No"}`};
+        
     const mensajePedido = `
-    <strong>Color:</strong> ${colorSeleccionado}<br>
-    <strong>Accesorios:</strong> ${mensajeAccesorios}<br>
-    <strong>Precio Vehículo:</strong> $${subtotalColor}<br>
-    <strong>Precio accesorios:</strong> $${subtotalAccesorio}<br>
-    <strong>Precio final:</strong> $${total}`;    
+        <strong>Color:</strong> ${colorSeleccionado}<br>
+        <strong>Accesorios:</strong> ${mensajeAccesorios}<br>
+        <strong>Precio accesorios:</strong> $${subtotalAccesorio}<br>
+        <strong>Precio Vehículo:</strong> $${subtotalColor}<br>
+        <strong>Precio final:</strong> $${total}`;  
+
     infoPedido.innerHTML = mensajePedido;
+};
+
+function enviarMensajeWhatsapp(){
+    
 };
